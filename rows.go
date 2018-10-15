f006d5d4b1bc676fdf196aaddddb8e66f95729b8
hugo.rut <hugo.rut@infosum.com>
2018-10-15 10:06:40 +0100

    add interface support

--- rows.go
+++ rows.go
@@ -4,15 +4,141 @@
 
 package odbc
 
+import "C"
 import (
+	"database/sql"
 	"database/sql/driver"
 	"io"
+	"reflect"
 
 	"github.com/alexbrainman/odbc/api"
 )
 
+var (
+	scanTypeNullFloat = reflect.TypeOf(sql.NullFloat64{})
+	scanTypeNullInt   = reflect.TypeOf(sql.NullInt64{})
+	scanTypeRawBytes  = reflect.TypeOf(sql.RawBytes{})
+	scanTypeUnknown   = reflect.TypeOf(new(interface{}))
+)
+
 type Rows struct {
 	os *ODBCStmt
+}
+
+func (r *Rows) ColumnTypeScanType(index int) reflect.Type {
+	switch x := r.os.Cols[index].(type) {
+	case *BindableColumn:
+		return toSqlType(x.CType).scanType
+	case *NonBindableColumn:
+		return toSqlType(x.CType).scanType
+	}
+	return scanTypeUnknown
+}
+
+func (r *Rows) ColumnTypeDatabaseTypeName(index int) string {
+	switch x := r.os.Cols[index].(type) {
+	case *BindableColumn:
+		return toSqlType(x.CType).name
+	case *NonBindableColumn:
+		return toSqlType(x.CType).name
+	}
+	return ""
+}
+
+type sqlType struct {
+	name     string
+	scanType reflect.Type
+}
+
+func toSqlType(ct api.SQLSMALLINT) sqlType {
+	switch ct {
+	case api.SQL_C_CHAR:
+		return sqlType{
+			name:     "SQL_C_CHAR",
+			scanType: scanTypeRawBytes,
+		}
+	case api.SQL_C_LONG:
+		return sqlType{
+			name:     "SQL_C_LONG",
+			scanType: scanTypeNullInt,
+		}
+	case api.SQL_C_SHORT:
+		return sqlType{
+			name:     "SQL_C_SHORT",
+			scanType: scanTypeNullInt,
+		}
+	case api.SQL_C_FLOAT:
+		return sqlType{
+			name:     "SQL_C_FLOAT",
+			scanType: scanTypeNullFloat,
+		}
+	case api.SQL_C_DOUBLE:
+		return sqlType{
+			name:     "SQL_C_DOUBLE",
+			scanType: scanTypeNullFloat,
+		}
+	case api.SQL_C_NUMERIC:
+		return sqlType{
+			name:     "SQL_C_NUMERIC",
+			scanType: scanTypeNullInt,
+		}
+	case api.SQL_C_DATE:
+		return sqlType{
+			name:     "SQL_C_DATE",
+			scanType: scanTypeRawBytes,
+		}
+	case api.SQL_C_TIME:
+		return sqlType{
+			name:     "SQL_C_TIME",
+			scanType: scanTypeRawBytes,
+		}
+	case api.SQL_C_TYPE_TIMESTAMP:
+		return sqlType{
+			name:     "SQL_C_TYPE_TIMESTAMP",
+			scanType: scanTypeRawBytes,
+		}
+	case api.SQL_C_TIMESTAMP:
+		return sqlType{
+			name:     "SQL_C_TIMESTAMP",
+			scanType: scanTypeRawBytes,
+		}
+	case api.SQL_C_BINARY:
+		return sqlType{
+			name:     "SQL_C_BINARY",
+			scanType: scanTypeRawBytes,
+		}
+	case api.SQL_C_BIT:
+		return sqlType{
+			name:     "SQL_C_BIT",
+			scanType: scanTypeRawBytes,
+		}
+	case api.SQL_C_WCHAR:
+		return sqlType{
+			name:     "SQL_C_WCHAR",
+			scanType: scanTypeRawBytes,
+		}
+	case api.SQL_C_DEFAULT:
+		return sqlType{
+			name:     "SQL_C_DEFAULT",
+			scanType: scanTypeRawBytes,
+		}
+	case api.SQL_C_SBIGINT:
+		return sqlType{
+			name:     "SQL_C_SBIGINT",
+			scanType: scanTypeNullInt,
+		}
+	case api.SQL_C_UBIGINT:
+		return sqlType{
+			name:     "SQL_C_UBIGINT",
+			scanType: scanTypeNullInt,
+		}
+	case api.SQL_C_GUID:
+		return sqlType{
+			name:     "SQL_C_GUID",
+			scanType: scanTypeNullInt,
+		}
+	}
+	return sqlType{scanType: scanTypeUnknown}
 }
 
 func (r *Rows) Columns() []string {
